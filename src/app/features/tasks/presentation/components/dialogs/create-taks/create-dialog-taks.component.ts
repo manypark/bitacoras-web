5c1dca42e6425063e09e272f781496c2a7338662
Manu <manypark@live.com>
2025-10-22 21:04:17 -0600

    feat: create dialog task

---
+++ src/app/features/tasks/presentation/components/dialogs/create-taks/create-dialog-taks.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector    : 'create-dialog-task',
+  templateUrl : './create-dialog-taks.component.html',
+  styleUrl    : './create-dialog-taks.component.css',
+})
+export class CreateTaksComponent {
+
+  submitNewTask() {
+    
+  }
+  
+  onClose() {
+
+  }
+}
--- src/app/features/tasks/presentation/pages/tasks/tasks.component.ts
+++ src/app/features/tasks/presentation/pages/tasks/tasks.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
 
+import { TitleDescriptionCustomButtonComponent } from "@app/shared";
+import { CreateTaksComponent } from "../../components/dialogs";
+
+const importsList = [TitleDescriptionCustomButtonComponent, CreateTaksComponent];
+
 @Component({
   selector    : 'app-tasks',
-  imports     : [],
+  imports     : [...importsList],
   templateUrl : './tasks.component.html',
   styleUrl    : './tasks.component.css',
 })
-export default class TasksComponent {}
+export default class TasksComponent {
+
+}
