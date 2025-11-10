0a84bdcb9c6ad08dfab3a7e3e701554ad45f40e2
Manu <manypark@live.com>
2025-11-10 11:21:05 -0600

    feat: install and configurate chart js

--- src/app/features/dashboards/presentation/pages/dashboard/dashboard.component.ts
+++ src/app/features/dashboards/presentation/pages/dashboard/dashboard.component.ts
@@ -1,16 +1,32 @@
 import { Component } from '@angular/core';
 
-import { LogsInfoComponent, RolesInfoComponent, TasksInfoComponent, UsersInfoComponent } from '@app/dashboards/presentation/components';
+import { ChartConfiguration } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
+import { Chart, registerables } from 'chart.js';
+
+import { LogsInfoComponent, RolesInfoComponent, TasksInfoComponent, UsersInfoComponent } from '@app/dashboards/presentation/components/containers-info';
+
+Chart.register(...registerables);
 
 @Component({
   selector    : 'app-dashboard',
   styleUrl    : './dashboard.component.css',
   templateUrl : './dashboard.component.html',
   imports     : [
+    LogsInfoComponent,
     UsersInfoComponent,
     TasksInfoComponent,
     RolesInfoComponent,
-    LogsInfoComponent
+    BaseChartDirective,
   ],
 })
-export default class DashboardComponent { }
+export default class DashboardComponent {
+  public barChartData: ChartConfiguration<'bar'>['data'] = {
+    labels: ['Enero', 'Febrero', 'Marzo', 'Abril'],
+    datasets: [{ data: [45, 37, 60, 70], label: 'Ventas' }],
+  };
+
+  public barChartOptions: ChartConfiguration<'bar'>['options'] = {
+    responsive: true,
+  };
+}
