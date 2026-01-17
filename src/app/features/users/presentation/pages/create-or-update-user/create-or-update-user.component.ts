4a3d7c722d7787b84b486306aebc138a0693b82f
Manu <manypark@live.com>
2026-01-17 00:22:01 -0600

    feat: add input file image preview and selection role list

--- src/app/features/users/presentation/pages/create-or-update-user/create-or-update-user.component.ts
+++ src/app/features/users/presentation/pages/create-or-update-user/create-or-update-user.component.ts
@@ -1,5 +1,7 @@
-import { Component, inject, OnInit } from '@angular/core';
+  import { Component, ElementRef, inject, OnInit, signal, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { GetAllRolesUsecase } from '@app/roles/domain';
+import { injectQuery } from '@tanstack/angular-query-experimental';
 
 @Component({
   selector    : 'create-or-update-user',
@@ -9,11 +11,28 @@ import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angula
   ]
 })
 export default class CreateOrUpdateUserComponent implements OnInit {
-
-  
+  // #=============== dependencias ===============#
   private fb  = inject(FormBuilder);
+  private getAllRolesUsecase = inject(GetAllRolesUsecase);
+
+  // #=============== variables ===============#
   createOrUpdateUserForm!:FormGroup;
+  imagePreview = signal<string>('');
+  selectedFile = signal<File | null>(null);
+  @ViewChild('fileInput') fileInput!: ElementRef<HTMLInputElement>;
+
+  // #=============== ciclo de vida ===============#
+  ngOnInit(): void {
+    this.initForm();
+  }
   
+  // #=============== queries ===============#
+  rolesList = injectQuery( () => ({
+    queryKey: ['rolesList'],
+    queryFn : () => this.getAllRolesUsecase.execute( 100, 0 ),
+  }));
+  
+  // #=============== funciones ===============#
   initForm() {
     this.createOrUpdateUserForm = this.fb.group({
       name      : ['', Validators.required],
@@ -24,7 +43,30 @@ export default class CreateOrUpdateUserComponent implements OnInit {
     });
   }
 
-  ngOnInit(): void {
-    this.initForm();
+  onFileSelected( event:Event ) {
+    const input = event.target as HTMLInputElement;
+
+    if (!input.files || input.files.length === 0) {
+      this.selectedFile.set(null);
+      return;
+    }
+
+    const file = input.files[0];
+
+    if (!file.type.startsWith('image/')) return;
+
+    this.selectedFile.set(file);
+
+    this.imagePreview.set( URL.createObjectURL(file) );
+  }
+
+  removeImage(): void {
+    this.selectedFile.set(null);
+
+    if (this.imagePreview) {
+      URL.revokeObjectURL( this.imagePreview() );
+      this.imagePreview.set('');
+      this.fileInput.nativeElement.value = '';
+    }
   }
 }
